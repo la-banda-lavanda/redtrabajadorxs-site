c6cba8c5bb38ed18f84ec79a4f6d8dc97d66510c
jonathanq27 <85052389+jonathanq27@users.noreply.github.com>
2022-07-20 19:18:48 -0500

    seciones

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
 import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+function MyApp({Component, pageProps}) {
+  const ThemeObjet = {
+    palette:{
+      type:'light', 
+      primary: {
+        main:'#EF5350',
+        
+      },
+      secundary:{
+        main: '#B71C1C',
+      },
+      background:{
+        default:'#EDE7F3',
+        paper:'#ed6d6d',
+      },
+    },
+    Typography:{
+      fontFamily: '"bebas"', 
+      h1:{
+        fontWeight: 500,
+      },
+    },
+  }
+  const theme=createTheme(ThemeObjet)
+  return (
+    <ThemeProvider theme={theme} >
+      <Component {...pageProps}/>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/vision-mision.js
@@ -0,0 +1,95 @@
+import *as React from 'react';
+
+import {Box,Typography,Container}from '@mui/material';
+import Image from 'next/image'
+import img1 from '../pages/img/imagen1.png'
+
+
+
+export default function vision_mision() {
+
+   
+   
+    return(
+
+         
+        <Container color="primary   " >
+           
+            <Box mt={2} 
+            sx={{display: 'inline-flex'}}
+            
+            >
+                
+                    <Image
+                src={img1}
+                width={400}
+                height={300} 
+            />
+               
+                 
+           
+
+            
+
+                
+             <Container sx={{mt:14 ,mb:20}}>
+                <Typography   aling="center  " >
+            Organizamos para garantizar el ejercicio efectivo de los
+            Derechos Humanos Laborales (DDHHL) y atender situaciones 
+            de desigualdad y relaciones inequitativas basadas en raza,
+            género y clase que impiden que las personas trabajadoras 
+            mexicanas tengan pleno acceso y el ejercicio de sus 
+            derechos. Trabajamos para incidir en la generación de 
+            espacios  para el acceso a la justicia de las personas 
+            trabajadoras, por lo cual, en el desarrollo de nuestros 
+            proyectos tenemos como ejes transversales la perspectiva 
+            de Derechos Humanos y de Género. Con nuestro acompaña-
+            miento y acciones buscamos que las personas trabajadoras 
+            adquieran herramientas para revertir relaciones sociales
+            asimétricas,  reducir  las desigualdades en el mundo del 
+            trabajo y fomentar un mundo con más justicia social.
+
+            </Typography>
+             </Container>
+            
+            </Box>
+            
+                <Box>
+
+                    <image
+            src={img1}
+            width={400}
+                height={300} 
+
+            s/>
+                </Box>
+                <Container>
+                    <Typography>
+            Organizamos para garantizar el ejercicio efectivo de los
+            Derechos Humanos Laborales (DDHHL) y atender situaciones 
+            de desigualdad y relaciones inequitativas basadas en raza,
+            género y clase que impiden que las personas trabajadoras 
+            mexicanas tengan pleno acceso y el ejercicio de sus 
+            derechos. Trabajamos para incidir en la generación de 
+            espacios  para el acceso a la justicia de las personas 
+            trabajadoras, por lo cual, en el desarrollo de nuestros 
+            proyectos tenemos como ejes transversales la perspectiva 
+            de Derechos Humanos y de Género. Con nuestro acompaña-
+            miento y acciones buscamos que las personas trabajadoras 
+            adquieran herramientas para revertir relaciones sociales
+            asimétricas,  reducir  las desigualdades en el mundo del 
+            trabajo y fomentar un mundo con más justicia social.
+            </Typography>
+
+                </Container>
+    
+        
+
+        </Container>
+        
+
+
+        
+    );
+    
+};
